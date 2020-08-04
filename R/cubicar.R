cfb06eb0eb88faefa30866751081605615fffbee
LiNk-NY <mramos09@gmail.com>
2020-08-04 21:45:28 +0000

    nueva funcion

---
+++ R/cubicar.R
@@ -0,0 +1,15 @@
+#' Cubicar un numero
+#'
+#' @param n Un numero  
+#'
+#' @return Un numero cubicado
+#' 
+#' @export
+#'
+#' @examples
+#' 
+#' cubicar(2)
+#' 
+cubicar <- function(n) {
+    n^3
+}
